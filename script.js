fd7825e0dc17b2b05bba18f840ebe3589cccfa8e
Yazmin Ramirez <yazzy06ramirez@aol.com>
2026-07-08 14:24:52 -0400

    writing starter code

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 //variables
 const PI = 3.1415;
 const radius = 5;
-const startingPlants = 20;
 
 const area = PI * radius * radius;
 console.log("Garden area", area);
 
-const plantSpace = 0.8
+//plants
+const plantSpace = 0.8;
+const startingPlants = 20;
+
+//week growth
+let week = 1;
+
+//plant weekly double growth
+let plantCount = startingPlants * (2 ** week);
+
+
+//PHUDOCODE! Write out your progress
