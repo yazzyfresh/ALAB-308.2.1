9120c4cd76228e2e06526792300238f232242e95
Yazmin Ramirez <yazzy06ramirez@aol.com>
2026-07-08 14:16:08 -0400

    establishes variables

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//variables
 const PI = 3.1415;
 const radius = 5;
-const startingPlants
+const startingPlants = 20;
+
+const area = PI * radius * radius;
+console.log("Garden area", area);
+
+const plantSpace = 0.8
