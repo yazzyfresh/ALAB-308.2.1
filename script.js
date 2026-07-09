bf58c3da8bf4bcab5bfbd244ec4abb154ef90da6
Yazmin Ramirez <yazzy06ramirez@aol.com>
2026-07-08 20:54:48 -0400

    added the if else in the for loop

--- script.js
+++ script.js
@@ -12,21 +12,29 @@ const startingPlants = 20;
 let week = 1;
 
 //plant weekly double growth
-let plantCount = startingPlants * (2 ** week);
+let plantCount = startingPlants * 2 ** week;
 
 //Area / Max Capacity of the plants
 
 let maxCapacity = area / minSpacePerPlant;
 
 console.log("Garden area", area);
-console.log("Maximum capacity" , maxCapacity);
+console.log("Maximum capacity", maxCapacity);
 
-// weeks making a loop for usablility 
+// weeks making a loop for usablility
 
-for (let week =1; week <=3; week++){
-    let plantCount = startingPlants * 2 ** week;
-    console.log("Week:" + week);
-    console.log(plantCount);
+for (let week = 1; week <= 3; week++) {
+  let plantCount = startingPlants * 2 ** week;
 
+  console.log("Week:" + week);
+  console.log(plantCount);
 
+  //===================pruned?
+  if (plantCount > maxCapacity * 0.8) {
+    console.log("Recomend : pruned");
+  } else if (plantCount >= maxCapacity * 0.5) {
+    console.log("recomend : mmonator");
+  } else {
+    console.log("recomed: planted");
+  }
 }
